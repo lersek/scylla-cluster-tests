08636e9d344761ff8d9ebab5a97a53bf743389a2
Laszlo Ersek <laszlo.ersek@scylladb.com>
2024-11-19 13:12:33 +0100

    hack(raft): multiply GCE backend timeouts by 10
    Signed-off-by: Laszlo Ersek <laszlo.ersek@scylladb.com>

--- sdcm/utils/raft/__init__.py
+++ sdcm/utils/raft/__init__.py
@@ -44,7 +44,7 @@ class MessageTimeout(NamedTuple):
 
 BACKEND_TIMEOUTS: dict[str, Mapping[LogPosition, int]] = {
     "aws": {LogPosition.BEGIN: 300, LogPosition.END: 3600},
-    "gce": {LogPosition.BEGIN: 300, LogPosition.END: 3600},
+    "gce": {LogPosition.BEGIN: 3000, LogPosition.END: 36000},
     "azure": {LogPosition.BEGIN: 1200, LogPosition.END: 7200},
 }
 
